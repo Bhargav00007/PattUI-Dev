2505f45b4fb2dcc482d5799031bd3a7426657b2a
Bhargav Pattanayak <122164885+Bhargav00007@users.noreply.github.com>
2024-09-07 23:19:39 +0530

    changes in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-      <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+      <Script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"/>
       </body>
     </html>
   );
